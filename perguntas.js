7f7861e437fa76af8e734377f6328a0ea6aec65b
nelsonxix90 <ramirez.nelson@escola.pr.gov.br>
2024-10-22 10:28:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+
+criaCartao(
+    'Esporte',
+    'Quanto tempo dura uma partida de futebol?',
+    'Quanto tempo dura uma partida de futebol',
+)
+ criaCartao(
+    'esporte',
+    'Quando foi realizada a primeira Copa do Mundo de futebol?',
+    'Em 13 de julho de 1930, no Uruguai.',
+ )
+ criaCartao(
+    'esporte',
+    'Quantos jogadores um time de futebol tem em jogo? ',
+    '11',
+ )
+ criaCartao(
+    'esporte',
+    'Quem ganhou a Copa do Mundo de 2010?',
+    'A seleção da Espanha.',
+
+ )
+
+    
+ 
